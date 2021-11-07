fix softmax and gradient test

gradient_test.py
@@ -2,45 +2,58 @@ import numpy as np
 import matplotlib.pyplot as plt
 from softmax import softmax_regression, softmax_grad
 from scipy.io import loadmat
+num_of_iterations = 10
+eps = [np.power(0.5, i) for i in range(num_of_iterations)]
+number_of_butches = 100
+
+def grad_W_test(X, W, bias, C):
+    cost = softmax_regression(X, W, bias, C)
+    grad_W, _ = softmax_grad(X, W, bias, C)
+    grad_flat = grad_W.flatten()
+
+    d = np.random.rand(grad_W.shape[0], grad_W.shape[1])
+    d /= np.linalg.norm(d)
+    d_flat = d.flatten()
 
 
-iter = 10
-eps = [np.power(0.5, i) for i in range(iter)]
-PeaksData = loadmat('PeaksData.mat')
-X = PeaksData.get("Yt")
-X = X[:,:100]
-C = PeaksData.get("Ct")
-C = C[:,:100]
-
-
-def grad_test(X, W, w_p, bias, c_p, C):
-	d = np.random.rand(W.shape[0], 1)
-	d /= np.linalg.norm(d)
-	F_0 = softmax_regression(X, W, bias, C)
-	grad_0 = softmax_grad(X, W, w_p, c_p, bias)
-	y_0 = []
-	y_1 = []
-	for epsilon in eps:
-		F_i = softmax_regression(X, W + epsilon*d, bias, C)
-		F_1 = F_0 + epsilon * d.T @ grad_0
-		y_0.append(abs(F_i - F_0))
-		y_1.append(abs(F_i - F_1))
-	return y_0, y_1
+    first_order = []
+    second_order = []
+    for epsilon in eps:
+        curr_cost = softmax_regression(X, W + epsilon * d, bias, C)
+        first_order.append(abs(curr_cost - cost))
+        second_order.append(abs(curr_cost - cost -epsilon * d_flat.T @ grad_flat))
+    return first_order, second_order
 
 
 def draw_results(y_0, y_1):
-	plt.semilogy(range(iter), y_0)
-	plt.semilogy(range(iter), y_1)
-	plt.legend("Zero order approx", "First order approx")
-	plt.title("Successful grad test in semilog scale plot")
-	plt.xlabel("iteration")
-	plt.ylabel("error")
-	plt.plot()
+    plt.semilogy([i for i in range(num_of_iterations)], y_0)
+    plt.semilogy([i for i in range(num_of_iterations)], y_1)
+    plt.legend(["First order approx", "Second order approx"])
+    plt.title("Successful grad test in semilog scale plot")
+    plt.xlabel("iteration")
+    plt.ylabel("error")
+    plt.show()
 
 
-W_0 = np.random.rand(2, 100)
-w_p = (W_0.T[0]).T
-c_p = (C.T[0]).T
+def extract_data():
+    peaks_data = loadmat("PeaksData.mat")
+    train_set = np.array(peaks_data['Yt'])
+    train_set_labels = np.array(peaks_data['Ct'])
 
-res_0, res_1 = grad_test(X, W_0, w_p, 0, c_p, C)
-draw_results(res_0, res_1)
+    idx = np.random.permutation(len(train_set_labels[0]))
+    train_set_X = train_set[:, idx]
+    train_set_C = train_set_labels[:, idx]
+
+    train_set_X_batches = np.array_split(train_set_X, number_of_butches, axis=1)
+    train_set_C_batches = np.array_split(train_set_C, number_of_butches, axis=1)
+
+    output_size = len(train_set_C_batches[0][:, 0])
+    input_size = train_set_X_batches[0].shape[0]
+    bias = np.zeros([1, output_size])
+    W = np.random.rand(input_size, output_size)
+    return train_set_X_batches, W, bias, train_set_C_batches
+
+if __name__ == '__main__':
+    X_batches, W, bias, C_batches = extract_data()
+    first_order, second_order = grad_W_test(X_batches[0], W, bias,  C_batches[0])
+    draw_results(first_order, second_order)

softmax.py
@@ -1,31 +1,23 @@
 import numpy as np
 
 
-def softmax_regression(X_L, W, bias, c):
-    prob = sigmoid(X_L, W)
+def softmax_regression(X_L, W, bias, C):
+    probs = sigmoid(X_L, W, bias)
     m = X_L.shape[1]
-    cost = (-1 / m) * (np.sum(c.T * np.log(prob), axis=0))
+    cost = (-1 / m) * (np.sum(C.T * np.log(probs)))
     return cost
 
 
-def sigmoid(X_L, W):
-    output_layer = X_L.T @ W
+def sigmoid(X_L, W, bias):
+    output_layer = X_L.T @ W + bias
     output_layer -= np.max(output_layer)
-    probability = np.exp(output_layer) / np.sum(np.exp(output_layer), axis=1)
-    return probability
+    probs = (np.exp(output_layer).T / np.sum(np.exp(output_layer), axis=1)).T
+    return probs
 
 
-def softmax_jacobian(X_L, W, c):
-    m = X_L.shape[1]
-    prob = sigmoid(X_L, W)
-    jacobian = (-1/m)*X_L@(prob - c.T)
-    return jacobian
-
-
-def softmax_grad(X, W, w_p, c_p, bias):
+def softmax_grad(X, W, bias, C):
+    probs = sigmoid(X, W, bias)
     m = X.shape[1]
-    Xtw_p = X.T @ w_p
-    XtW = X.T @ W
-    temp = np.exp(Xtw_p) / np.sum(np.exp(XtW), axis=1)
-    grad = (1 / m) * X @ (temp - c_p)
-    return grad
+    grad_W = (-1 / m) * (X @ (C.T - probs))
+    grad_b = (-1 / m) * (np.sum(C.T - probs, axis=0)).T
+    return grad_W, grad_b