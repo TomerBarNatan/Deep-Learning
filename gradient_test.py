423ff2c91fb1bb555483ae9c5bb0fab3389751f1
TalBN94 <talbarnathan@gmail.com>
2021-11-08 12:29:35 +0200

    renamed some variables to match Eran's notes, added bias grad test, made some functions more generic

--- gradient_test.py
+++ gradient_test.py
@@ -1,59 +1,72 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from softmax import softmax_regression, softmax_grad
-from scipy.io import loadmat
+from loadData import extract_data
+
 num_of_iterations = 10
-eps = [np.power(0.5, i) for i in range(num_of_iterations)]
-number_of_butches = 100
+
 
 def grad_W_test(X, W, bias, C):
-    cost = softmax_regression(X, W, bias, C)
+    # calculate F(W) and the gradient w.r.t W
+    F_0 = softmax_regression(X, W, bias, C)
     grad_W, _ = softmax_grad(X, W, bias, C)
-    grad_flat = grad_W.flatten()
+    grad_W_flat = grad_W.flatten()
 
+    # define a random d for the test
     d = np.random.rand(grad_W.shape[0], grad_W.shape[1])
     d /= np.linalg.norm(d)
     d_flat = d.flatten()
 
-
+    zero_order = []
     first_order = []
-    second_order = []
-    for epsilon in eps:
-        curr_cost = softmax_regression(X, W + epsilon * d, bias, C)
-        first_order.append(abs(curr_cost - cost))
-        second_order.append(abs(curr_cost - cost -epsilon * d_flat.T @ grad_flat))
-    return first_order, second_order
+    print('\nGradient test w.r.t W results:')
+    print('k\t\terror order 0\t\terror order 1')
+    for k in range(num_of_iterations):
+        epsilon = 0.5 ** k
+        F_k = softmax_regression(X, W + epsilon * d, bias, C)
+        F_1 = F_0 + epsilon * d_flat.T @ grad_W_flat
+        zero_order.append(abs(F_k - F_0))
+        first_order.append(abs(F_k - F_1))
+        print(k, '\t', abs(F_k - F_0), '\t', abs(F_k - F_1))
+    return zero_order, first_order
 
 
-def draw_results(y_0, y_1):
+def grad_bias_test(X, W, bias, C):
+    # calculate F(W) and the gradient w.r.t bias
+    F_0 = softmax_regression(X, W, bias, C)
+    _, grad_bias = softmax_grad(X, W, bias, C)
+
+    # define a random d for the test
+    d = np.random.rand(grad_bias.shape[0])
+    d /= np.linalg.norm(d)
+
+    zero_order = []
+    first_order = []
+    print('\nGradient test w.r.t bias results:')
+    print('k\t\terror order 0\t\terror order 1')
+    for k in range(num_of_iterations):
+        epsilon = 0.5 ** k
+        F_k = softmax_regression(X, W, bias + epsilon * d, C)
+        F_1 = F_0 + epsilon * d.T @ grad_bias
+        zero_order.append(abs(F_k - F_0))
+        first_order.append(abs(F_k - F_1))
+        print(k, '\t', abs(F_k - F_0), '\t', abs(F_k - F_1))
+    return zero_order, first_order
+
+
+def draw_results(y_0, y_1, result_for='weights'):
     plt.semilogy([i for i in range(num_of_iterations)], y_0)
     plt.semilogy([i for i in range(num_of_iterations)], y_1)
-    plt.legend(["First order approx", "Second order approx"])
-    plt.title("Successful grad test in semilog scale plot")
+    plt.legend(["Zero order approx", "First order approx"])
+    plt.title("Successful {res_for} gradient test in semilog scale".format(res_for=result_for))
     plt.xlabel("iteration")
     plt.ylabel("error")
     plt.show()
 
 
-def extract_data():
-    peaks_data = loadmat("PeaksData.mat")
-    train_set = np.array(peaks_data['Yt'])
-    train_set_labels = np.array(peaks_data['Ct'])
-
-    idx = np.random.permutation(len(train_set_labels[0]))
-    train_set_X = train_set[:, idx]
-    train_set_C = train_set_labels[:, idx]
-
-    train_set_X_batches = np.array_split(train_set_X, number_of_butches, axis=1)
-    train_set_C_batches = np.array_split(train_set_C, number_of_butches, axis=1)
-
-    output_size = len(train_set_C_batches[0][:, 0])
-    input_size = train_set_X_batches[0].shape[0]
-    bias = np.zeros([1, output_size])
-    W = np.random.rand(input_size, output_size)
-    return train_set_X_batches, W, bias, train_set_C_batches
-
 if __name__ == '__main__':
-    X_batches, W, bias, C_batches = extract_data()
-    first_order, second_order = grad_W_test(X_batches[0], W, bias,  C_batches[0])
-    draw_results(first_order, second_order)
+    X_batches, W, bias, C_batches = extract_data("PeaksData.mat", 100)
+    zero_order_W, first_order_W = grad_W_test(X_batches[0], W, bias, C_batches[0])
+    draw_results(zero_order_W, first_order_W)
+    zero_order_bias, first_order_bias = grad_bias_test(X_batches[0], W, bias, C_batches[0])
+    draw_results(zero_order_bias, first_order_bias, result_for='bias')
