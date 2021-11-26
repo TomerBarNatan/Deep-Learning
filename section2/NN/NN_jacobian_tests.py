870f87ddad9f837ae17783c18ddbd8bad0428da9
tomer.bar.natan2 <tomer.bar.natan2@gmail.com>
2021-11-26 17:57:08 +0200

    gradient test whole network

--- section2/NN/NN_jacobian_tests.py
+++ section2/NN/NN_jacobian_tests.py
@@ -13,7 +13,7 @@ def jacobian_test_layer_X(nn: NN, X_0):
     diff = np.zeros(iter_num)
     diff_grad = np.zeros(iter_num)
     epsilons = [0.5 ** i for i in range(iter_num)]
-    d = np.random.rand(*X.shape)
+    d = np.ones((X.shape[0], X.shape[1]))
     d /= np.linalg.norm(d)
     W = nn.weights[1]
     b = nn.biases[1]
@@ -53,7 +53,8 @@ def jacobian_test_layer_W(nn: NN, X_0):
     diff = np.zeros(iter_num)
     diff_grad = np.zeros(iter_num)
     epsilons = [0.5 ** i for i in range(iter_num)]
-    d = np.random.rand(*W.shape)
+    d = np.ones((W.shape[0], W.shape[1]))
+    # d = np.random.rand(*W.shape)
     d /= np.linalg.norm(d)
 
     X_linear, X_forward = nn.forward_step(X, W, b)
@@ -61,7 +62,7 @@ def jacobian_test_layer_W(nn: NN, X_0):
     g_x = np.dot(X_forward_T, U).item()
 
     D = np.diag(nn.activation_gradient(X_linear).flatten())
-    Jx = D @ np.kron(X.T, np.eye(int(D.shape[1]/X.T.shape[0])))
+    Jx = D @ np.kron(X.T, np.eye(int(D.shape[1] / X.T.shape[0])))
     g_grad = Jx.T @ U
     for i, epsilon in enumerate(epsilons):
         W_diff = W.copy()
@@ -81,8 +82,48 @@ def jacobian_test_layer_W(nn: NN, X_0):
     plt.show()
 
 
+def jacobian_test_layer_b(nn: NN, X_0):
+    iter_num = 10
+    _, X = nn.forward_step(X_0, nn.weights[0], nn.biases[0])
+    W = nn.weights[1]
+    b = nn.biases[1]
+    n, m = X.shape
+    out_dimensions = nn.biases[1].shape[0]
+    U = np.random.rand(out_dimensions, m)
+    diff = np.zeros(iter_num)
+    diff_grad = np.zeros(iter_num)
+    epsilons = [0.5 ** i for i in range(iter_num)]
+    d = np.random.rand(b.shape[0], b.shape[1])
+    d /= np.linalg.norm(d)
+
+    X_linear, X_forward = nn.forward_step(X, W, b)
+    X_forward_T = X_forward.T
+    g_x = np.dot(X_forward_T, U).item()
+
+
+    Jb = np.diag(nn.activation_gradient(X_linear).flatten())
+    g_grad = Jb.T @ U
+    for i, epsilon in enumerate(epsilons):
+        b_diff = b.copy()
+        b_diff += d * epsilon
+        _, X_eps_forward = nn.forward_step(X, W, b_diff)
+        X_eps_forward_T = X_eps_forward.T
+        gx_epsilon = np.dot(X_eps_forward_T, U).item()
+        d_flat = d.reshape(-1, 1)
+        diff[i] = abs(gx_epsilon - g_x)
+        diff_grad[i] = abs(gx_epsilon - g_x - epsilon * d_flat.T @ g_grad)
+    plt.semilogy(np.arange(1, iter_num + 1, 1), diff)
+    plt.semilogy(np.arange(1, iter_num + 1, 1), diff_grad)
+    plt.xlabel('epsilons')
+    plt.ylabel('difference')
+    plt.title('W Jacobian Test Results')
+    plt.legend(("diff without grad", "diff with grad"))
+    plt.show()
+
+
 if __name__ == '__main__':
     nn = NN([3, 5, 7, 8], tanh, tanh_grad)
     X = np.random.rand(3, 1)
+    jacobian_test_layer_b(nn, X)
     # jacobian_test_layer_X(nn, X)
-    jacobian_test_layer_W(nn, X)
+    # jacobian_test_layer_W(nn, X)
--- section2/NN/nn_gradient_test.py
+++ section2/NN/nn_gradient_test.py
@@ -2,23 +2,50 @@ import numpy as np
 from section2.NN.NN import NN
 from section2.activations import tanh, tanh_grad
 import matplotlib.pyplot as plt
-num_of_iterations = 30
+
+num_of_iterations = 15
 
 
-def nn_grad_test(nn: NN, X):
+def nn_grad_test_W(nn: NN, X):
     C = np.zeros((10, 1))
     C[0] = 1
     F_0, _, _, nonlinear_layers = nn.forward(X, C)
     weight_grads, biases_grads = nn.backpropagation(nonlinear_layers, C)
+    grad_W = np.concatenate([w.flatten() for w in weight_grads])
+
     # define a random d for the test
-    ds_w = [np.random.rand(w.shape[0],w.shape[1]) for w in nn.weights]
-    ds_b = [np.random.rand(b.shape[0],b.shape[1]) for b in nn.biases]
-    weight_grads_flatten = np.concatenate([w.flatten() for w in weight_grads])
-    biases_grads_flatten = np.concatenate([b.flatten() for b in biases_grads])
-    grads_flatten = np.concatenate([weight_grads_flatten,biases_grads_flatten])
-    d_flat = np.random.rand(grads_flatten.shape[0], 1)
-    d_flat /= np.linalg.norm(d_flat)
+    ds_w = [np.ones((w.shape[0], w.shape[1])) for w in nn.weights]
+    ds_w = [d / np.linalg.norm(d) for d in ds_w]
+    d_flat = np.concatenate([d.flatten() for d in ds_w])
     nn_weights_original = nn.weights.copy()
+
+    zero_order = []
+    first_order = []
+    print('\nGradient test w.r.t bias results:')
+    print('k\t\terror order 0\t\terror order 1')
+    for k in range(num_of_iterations):
+        epsilon = 0.5 ** k
+        nn.weights = [nn.weights[i] + epsilon * ds_w[i] for i in range(len(nn.weights))]
+        F_k, _, _, _ = nn.forward(X, C)
+        F_1 = F_0 + epsilon * d_flat.T @ grad_W
+        zero_order.append(np.linalg.norm(F_k - F_0))
+        first_order.append(np.linalg.norm((F_k - F_1)))
+        print(k, '\t', np.linalg.norm(F_k - F_0), '\t', np.linalg.norm(F_k - F_1))
+        nn.weights = nn_weights_original
+    draw_results(zero_order, first_order)
+
+
+def nn_grad_test_b(nn: NN, X):
+    C = np.zeros((10, 1))
+    C[0] = 1
+    F_0, _, _, nonlinear_layers = nn.forward(X, C)
+    _, biases_grads = nn.backpropagation(nonlinear_layers, C)
+    grad_b = np.concatenate([b.flatten() for b in biases_grads])
+
+    # define a random d for the test
+    ds_b = [np.random.rand(b.shape[0], b.shape[1]) for b in nn.biases]
+    ds_b = [d / np.linalg.norm(d) for d in ds_b]
+    d_flat = np.concatenate([d.flatten() for d in ds_b])
     nn_biases_original = nn.biases.copy()
 
     zero_order = []
@@ -27,16 +54,14 @@ def nn_grad_test(nn: NN, X):
     print('k\t\terror order 0\t\terror order 1')
     for k in range(num_of_iterations):
         epsilon = 0.5 ** k
-        nn.weights = [nn.weights[i] + epsilon* ds_w[i] for i in range(len(nn.weights))]
-        nn.biases = [nn.biases[i] + epsilon* ds_b[i] for i in range(len(nn.biases))]
-        F_k, _, _, _ = nn.forward(X,C)
-        F_1 = F_0 + epsilon * d_flat.T @ grads_flatten
+        nn.biases = [nn.biases[i] + epsilon * ds_b[i] for i in range(len(nn.biases))]
+        F_k, _, _, _ = nn.forward(X, C)
+        F_1 = F_0 + epsilon * d_flat.T @ grad_b
         zero_order.append(np.linalg.norm(F_k - F_0))
-        first_order.append(np.linalg.norm((F_k - F_1) ** 2))
+        first_order.append(np.linalg.norm((F_k - F_1)))
         print(k, '\t', np.linalg.norm(F_k - F_0), '\t', np.linalg.norm(F_k - F_1))
-        nn.weights = nn_weights_original
         nn.biases = nn_biases_original
-    return zero_order, first_order
+    draw_results(zero_order, first_order, "Biases")
 
 
 def draw_results(y_0, y_1, result_for='Weights'):
@@ -50,7 +75,7 @@ def draw_results(y_0, y_1, result_for='Weights'):
 
 
 if __name__ == '__main__':
-    nn = NN([50, 10,10,10,10], tanh, tanh_grad)
+    nn = NN([50, 10, 10, 10, 10], tanh, tanh_grad)
     X = np.random.rand(50, 1)
-    zero_order_bias, first_order_bias = nn_grad_test(nn, X)
-    draw_results(zero_order_bias, first_order_bias, result_for='Weights')
+    nn_grad_test_W(nn, X)
+    nn_grad_test_b(nn, X)
