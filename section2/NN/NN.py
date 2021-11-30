Correct jacobian tests for NN in file test.py

section2/NN/NN.py
@@ -20,8 +20,6 @@ class NN:
         for i in range(len(network_layers_list) - 1):
             W_i = np.random.uniform(-limit, limit, size=(network_layers_list[i + 1], network_layers_list[i]))
             W_i /= np.linalg.norm(W_i)
-            # W_i = np.zeros((network_layers_list[i + 1], network_layers_list[i]))
-            # W_i[1:2] = 1
             bias_i = np.zeros([network_layers_list[i + 1], 1])
             self.weights.append(W_i)
             self.biases.append(bias_i)
@@ -106,8 +104,8 @@ class NN:
         common = grad_activation * v
         grad_W = common @ X.T
         grad_b = np.sum(common, axis=1, keepdims=True)
-        new_v = W.T @ common
-        return grad_W, grad_b, new_v
+        grad_X = W.T @ common
+        return grad_W, grad_b, grad_X
 
     def backpropagation(self, X_list, C):
         """
@@ -117,20 +115,23 @@ class NN:
         :return: gradients list of each layer w.r.t weights and bias
         """
         layer_number = len(X_list)
+        x_grads = []
         weights_grads = []
         biases_grads = []
 
         # last layer gradient
         W_i_grad, b_i_grad, v_i = self.backward_last_layer(X_list, C)
+        x_grads.insert(0, v_i)
         weights_grads.insert(0, W_i_grad)
         biases_grads.insert(0, b_i_grad)
 
         # hidden layer grads
         for i in range(layer_number - 2, 0, -1):
             W_i_grad, b_i_grad, v_i = self.backward_hidden_layer(X_list, i, v_i)
+            x_grads.insert(0, v_i)
             weights_grads.insert(0, W_i_grad)
             biases_grads.insert(0, b_i_grad)
-        return weights_grads, biases_grads
+        return x_grads, weights_grads, biases_grads
 
     def backward_last_layer(self, X_list, C):
         """
@@ -151,8 +152,8 @@ class NN:
         :param v: vector v from the previous backward step
         :return: New vector v for the next backward step to use
         """
-        F_grad_W_i, F_grad_b_i, v_new = self.hidden_layer_grad(X_list[i - 1], self.weights[i - 1], self.biases[i - 1], v)
-        return F_grad_W_i, F_grad_b_i, v_new
+        F_grad_W_i, F_grad_b_i, F_grad_X_i = self.hidden_layer_grad(X_list[i - 1], self.weights[i - 1], self.biases[i - 1], v)
+        return F_grad_W_i, F_grad_b_i, F_grad_X_i
 
     def update_thetas(self, weights_grads, biases_grads, learning_rate):
         """

section2/NN/nn_gradient_test.py
@@ -16,7 +16,7 @@ def nn_grad_test_W(nn: NN, X):
     C = np.zeros((10, 1))
     C[0] = 1
     F_0, _, _, nonlinear_layers = nn.forward(X, C)
-    weight_grads, biases_grads = nn.backpropagation(nonlinear_layers, C)
+    _, weight_grads, biases_grads = nn.backpropagation(nonlinear_layers, C)
     grad_W = np.concatenate([w.flatten() for w in weight_grads])
 
     # define a random d for the test and align dimensions
@@ -51,7 +51,7 @@ def nn_grad_test_b(nn: NN, X):
     C = np.zeros((10, 1))
     C[0] = 1
     F_0, _, _, nonlinear_layers = nn.forward(X, C)
-    _, biases_grads = nn.backpropagation(nonlinear_layers, C)
+    _, _, biases_grads = nn.backpropagation(nonlinear_layers, C)
     grad_b = np.concatenate([b.flatten() for b in biases_grads])
 
     # define a random d for the test

section2/NN/test.py
@@ -13,33 +13,30 @@ def jacobian_test_layer_X(nn: NN, X_0):
     :param X_0: initial data (we init with random data)
     :return: shows a plot of the zero order vs. first order approximation
     """
-    _, X = nn.forward_step(X_0, nn.weights[0], nn.biases[0])
-    n, m = X.shape
-    out_dimensions = nn.biases[1].shape[0]
-    U = np.random.rand(out_dimensions, m)
+    W_0 = nn.weights[0].copy()
+    b_0 = nn.biases[0].copy()
+    _, X_1 = nn.forward_step(X_0, W_0, b_0)
+    n, m = X_1.shape
+    out_dimensions = b_0.shape[0]
+    u = np.random.rand(out_dimensions, m)
+    d = np.random.rand(*X_0.shape)
+    d = (1 / np.linalg.norm(d)) * d
+
+    g_x = np.dot(X_1.T, u).item()
+    _, _, JtU_X = nn.backward_hidden_layer([X_0, X_1], 1, u)
+
     zero_order = np.zeros(iter_num)
     first_order = np.zeros(iter_num)
     epsilons = [0.5 ** i for i in range(iter_num)]
-    d = np.ones((X.shape[0], X.shape[1]))
-    d /= np.linalg.norm(d)
-    W = nn.weights[1]
-    b = nn.biases[1]
-
-    X_linear, X_forward = nn.forward_step(X, W, b)
-    X_forward_T = X_forward.T
-    g_x = np.dot(X_forward_T, U).item()
-
-    Jx = np.diag(nn.activation_gradient(X_linear).flatten()) @ W
-    g_grad = Jx.T @ U
     for i, epsilon in enumerate(epsilons):
-        X_diff = X.copy()
+        X_diff = X_0.copy()
         X_diff += d * epsilon
-        _, X_eps_forward = nn.forward_step(X_diff, W, b)
+        _, X_eps_forward = nn.forward_step(X_diff, W_0, b_0)
         X_eps_forward_T = X_eps_forward.T
-        gx_epsilon = np.dot(X_eps_forward_T, U).item()
+        gx_epsilon = np.dot(X_eps_forward_T, u).item()
         d_flat = d.reshape(-1, 1)
         zero_order[i] = abs(gx_epsilon - g_x)
-        first_order[i] = abs(gx_epsilon - g_x - epsilon * d_flat.T @ g_grad)
+        first_order[i] = abs(gx_epsilon - g_x - epsilon * d_flat.T @ JtU_X)
     draw_results(zero_order, first_order)
 
 
@@ -50,34 +47,31 @@ def jacobian_test_layer_W(nn: NN, X_0):
         :param X_0: initial data (we init with random data)
         :return: shows a plot of the zero order vs. first order approximation
         """
-    _, X = nn.forward_step(X_0, nn.weights[0], nn.biases[0])
-    W = nn.weights[1]
-    b = nn.biases[1]
-    n, m = X.shape
-    out_dimensions = nn.biases[1].shape[0]
-    U = np.random.rand(out_dimensions, m)
+    W_0 = nn.weights[0].copy()
+    b_0 = nn.biases[0].copy()
+    _, X_1 = nn.forward_step(X_0, W_0, b_0)
+    n, m = X_1.shape
+    out_dimensions = b_0.shape[0]
+    u = np.random.rand(out_dimensions, m)
+    d = np.random.rand(*W_0.shape)
+    d = (1 / np.linalg.norm(d)) * d
+
+    g_x = np.dot(X_1.T, u).item()
+    JtU_W, _, _ = nn.backward_hidden_layer([X_0, X_1], 1, u)
+
     zero_order = np.zeros(iter_num)
     first_order = np.zeros(iter_num)
     epsilons = [0.5 ** i for i in range(iter_num)]
-    d = np.ones((W.shape[0], W.shape[1]))
-    d /= np.linalg.norm(d)
-
-    X_linear, X_forward = nn.forward_step(X, W, b)
-    X_forward_T = X_forward.T
-    g_x = np.dot(X_forward_T, U).item()
-
-    D = np.diag(nn.activation_gradient(X_linear).flatten())
-    Jx = D @ np.kron(X.T, np.eye(int(D.shape[1] / X.T.shape[0])))
-    g_grad = Jx.T @ U
     for i, epsilon in enumerate(epsilons):
-        W_diff = W.copy()
+        W_diff = W_0.copy()
         W_diff += d * epsilon
-        _, X_eps_forward = nn.forward_step(X, W_diff, b)
+        _, X_eps_forward = nn.forward_step(X_0, W_diff, b_0)
         X_eps_forward_T = X_eps_forward.T
-        gx_epsilon = np.dot(X_eps_forward_T, U).item()
+        gx_epsilon = np.dot(X_eps_forward_T, u).item()
         d_flat = d.reshape(-1, 1)
+        JtU_W_flat = JtU_W.reshape(-1, 1)
         zero_order[i] = abs(gx_epsilon - g_x)
-        first_order[i] = abs(gx_epsilon - g_x - epsilon * d_flat.T @ g_grad)
+        first_order[i] = abs(gx_epsilon - g_x - epsilon * d_flat.T @ JtU_W_flat)
     draw_results(zero_order, first_order, 'weights')
 
 
@@ -88,34 +82,30 @@ def jacobian_test_layer_b(nn: NN, X_0):
         :param X_0: initial data (we init with random data)
         :return: shows a plot of the zero order vs. first order approximation
         """
-    _, X = nn.forward_step(X_0, nn.weights[0], nn.biases[0])
-    W = nn.weights[1]
-    b = nn.biases[1]
-    n, m = X.shape
-    out_dimensions = nn.biases[1].shape[0]
-    U = np.random.rand(out_dimensions, m)
+    W_0 = nn.weights[0].copy()
+    b_0 = nn.biases[0].copy()
+    _, X_1 = nn.forward_step(X_0, W_0, b_0)
+    n, m = X_1.shape
+    out_dimensions = b_0.shape[0]
+    u = np.random.rand(out_dimensions, m)
+    d = np.random.rand(*b_0.shape)
+    d = (1 / np.linalg.norm(d)) * d
+
+    g_x = np.dot(X_1.T, u).item()
+    _, JtU_b, _ = nn.backward_hidden_layer([X_0, X_1], 1, u)
+
     zero_order = np.zeros(iter_num)
     first_order = np.zeros(iter_num)
     epsilons = [0.5 ** i for i in range(iter_num)]
-    d = np.random.rand(b.shape[0], b.shape[1])
-    d /= np.linalg.norm(d)
-
-    X_linear, X_forward = nn.forward_step(X, W, b)
-    X_forward_T = X_forward.T
-    g_x = np.dot(X_forward_T, U).item()
-
-    Jb = np.diag(nn.activation_gradient(X_linear).flatten())
-    g_grad = Jb.T @ U
     for i, epsilon in enumerate(epsilons):
-        b_diff = b.copy()
+        b_diff = b_0.copy()
         b_diff += d * epsilon
-        _, X_eps_forward = nn.forward_step(X, W, b_diff)
+        _, X_eps_forward = nn.forward_step(X_0, W_0, b_diff)
         X_eps_forward_T = X_eps_forward.T
-        gx_epsilon = np.dot(X_eps_forward_T, U).item()
-        d_flat = d.reshape(-1, 1)
+        gx_epsilon = np.dot(X_eps_forward_T, u).item()
         zero_order[i] = abs(gx_epsilon - g_x)
-        first_order[i] = abs(gx_epsilon - g_x - epsilon * d_flat.T @ g_grad)
-    draw_results(zero_order, first_order, 'biases')
+        first_order[i] = abs(gx_epsilon - g_x - epsilon * d.T @ JtU_b)
+    draw_results(zero_order, first_order, 'bias')
 
 
 def draw_results(zero_order, first_order, wrt='data'):
@@ -129,7 +119,7 @@ def draw_results(zero_order, first_order, wrt='data'):
 
 
 if __name__ == '__main__':
-    nn = NN([3, 5, 7], tanh, tanh_grad)
+    nn = NN([3, 5, 7, 8], tanh, tanh_grad)
     X = np.random.rand(3, 1)
     jacobian_test_layer_b(nn, X)
     jacobian_test_layer_X(nn, X)

section2/ResNet/residual_network.py
@@ -131,8 +131,8 @@ class ResNet:
         grad_W1 = (1 / batch_size) * (grad_activation * (Ws[1].T @ v)) @ X.T
         grad_W2 = (1 / batch_size) * v @ (self.activation(linear)).T
         grad_b = (1 / batch_size) * np.sum((grad_activation * (Ws[1].T @ v)), axis=1, keepdims=True)
-        new_v = v + (Ws[0].T @ (grad_activation * (Ws[1].T @ v)))
-        return grad_W1, grad_W2, grad_b, new_v
+        grad_X = v + (Ws[0].T @ (grad_activation * (Ws[1].T @ v)))
+        return grad_W1, grad_W2, grad_b, grad_X
 
     def hidden_layer_grad(self, X, W, b, v):
         """
@@ -149,8 +149,8 @@ class ResNet:
         common = grad_activation * v
         grad_W = (1 / batch_size) * common @ X.T
         grad_b = (1 / batch_size) * np.sum(common, axis=1, keepdims=True)
-        new_v = W.T @ common
-        return grad_W, grad_b, new_v
+        grad_X = W.T @ common
+        return grad_W, grad_b, grad_X
 
     def backpropagation(self, X_list, C):
         """
@@ -160,27 +160,39 @@ class ResNet:
         :return: gradients list of each layer w.r.t weights and bias
         """
         layer_number = len(X_list)
+        x_grads = []
         weight_grads = []
         bias_grads = []
 
         # last layer gradient
-        W_grad, b_grad, x_grad = self.softmax_gradient(X_list[-1], self.weights[-1], C, X_list[-2])
-        weight_grads.append(W_grad.copy())
-        bias_grads.append(b_grad.copy())
+        W_grad, b_grad, x_grad = self.backward_last_layer(X_list, C)
+        x_grads.insert(0, x_grad.copy())
+        weight_grads.insert(0, W_grad.copy())
+        bias_grads.insert(0, b_grad.copy())
         v_i = x_grad.copy()
 
         # hidden layer grads
         for i in range(layer_number - 2, 1, -1):
-            F_grad_W1_i, F_grad_W2_i, F_grad_b_i, v_i = self.res_hidden_layer_grad(X_list[i - 1], self.weights[i - 1],
-                                                                                   self.biases[i - 1],
-                                                                                   v_i)
-            weight_grads.append([F_grad_W1_i.copy(), F_grad_W2_i.copy()])
-            bias_grads.append(F_grad_b_i.copy())
+            F_grad_W1_i, F_grad_W2_i, F_grad_b_i, v_i = self.backward_hidden_layer(X_list, i, v_i)
+            x_grads.insert(0, v_i)
+            weight_grads.insert(0, [F_grad_W1_i.copy(), F_grad_W2_i.copy()])
+            bias_grads.insert(0, F_grad_b_i.copy())
 
-        grad_first_W, grad_first_b, _ = self.hidden_layer_grad(X_list[0], self.weights[0], self.biases[0], v_i)
-        weight_grads.append(grad_first_W.copy())
-        bias_grads.append(grad_first_b.copy())
-        return list(reversed(weight_grads)), list(reversed(bias_grads))
+        grad_first_W, grad_first_b, grad_first_X = self.hidden_layer_grad(X_list[0], self.weights[0], self.biases[0], v_i)
+        x_grads.insert(0, grad_first_X)
+        weight_grads.insert(0, grad_first_W.copy())
+        bias_grads.insert(0, grad_first_b.copy())
+        return x_grads, weight_grads, bias_grads
+
+    def backward_last_layer(self, X_list, C):
+        W_grad, b_grad, x_grad = self.softmax_gradient(X_list[-1], self.weights[-1], C, X_list[-2])
+        return W_grad, b_grad, x_grad
+
+    def backward_hidden_layer(self, X_list, i, v):
+        F_grad_W1_i, F_grad_W2_i, F_grad_b_i, grad_X_i = self.res_hidden_layer_grad(X_list[i - 1], self.weights[i - 1],
+                                                                               self.biases[i - 1],
+                                                                               v)
+        return F_grad_W1_i, F_grad_W2_i, F_grad_b_i, grad_X_i
 
     def update_thetas(self, W_grad_list, bias_grad_list, learning_rate):
         """